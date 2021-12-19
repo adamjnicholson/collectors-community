cfd4f99a6b80da3795361d8971c76c591b71b123
Adam Nicholson <adamn@live.com.au>
2021-12-19 22:09:56 +1000

    refactor validateForm function

--- app/routes/admin/brands/new.tsx
+++ app/routes/admin/brands/new.tsx
@@ -26,68 +26,62 @@ const slugSchema = z
         message: "A slug must be atleast 3 characters long",
     });
 
-type FieldNames<T extends ActionData> = keyof T["fields"];
-type FormSchema<T extends ActionData> = Record<
-    FieldNames<T>,
-    {
-        value: string | null;
-        schema: ZodTypeAny;
+const formSchema = z.object({
+    name: nameSchema,
+    slug: slugSchema,
+});
+
+const validateForm = <
+    Fields extends Record<string, string | number | boolean | null | undefined>,
+    Schema extends z.ZodObject<{
+        [Key in keyof Fields]: z.ZodType<NonNullable<Fields[Key]>>;
+    }>
+>(
+    fields: Fields,
+    schema: Schema
+) => {
+    const result = schema.safeParse(fields);
+
+    if (!result.success) {
+        return {
+            success: result.success,
+            fields,
+            fieldErrors: result.error.issues.reduce(
+                (fieldErrors, error) => ({
+                    ...fieldErrors,
+                    [error.path[0]]: error.message,
+                }),
+                fields
+            ),
+        };
     }
->;
 
-const validateForm = <T extends ActionData>(validator: FormSchema<T>) => {
-    const fieldNames = Object.keys(validator) as unknown as Array<
-        keyof typeof validator
-    >;
-
-    return fieldNames.reduce<Pick<T, "fields" | "fieldErrors">>(
-        (prev, curr) => {
-            const { value, schema } = validator[curr];
-            const parsedValue = schema.safeParse(value);
-
-            const fields = {
-                ...prev.fields,
-                [curr]: value,
-            };
-
-            const fieldErrors = {
-                ...prev.fieldErrors,
-                [curr]: parsedValue.success ? "" : parsedValue.error.message,
-            };
-
-            return { fields, fieldErrors };
-        },
-        {
-            fields: {},
-            fieldErrors: {},
-        }
-    );
+    return {
+        success: result.success,
+        fields: result.data,
+        fieldErrors: fields,
+    };
 };
-
-const isFormValid = <T extends ActionData>(fieldErrors: T["fieldErrors"]) =>
-    Object.values(fieldErrors ?? {}).some((error) => !!error);
 
 export const action: ActionFunction = async ({
     request,
 }): Promise<Response | ActionData> => {
     const body = new URLSearchParams(await request.text());
-    const name = body.get("name");
-    const slug = body.get("slug");
 
-    const { fields, fieldErrors } = validateForm<ActionData>({
-        name: {
-            value: name,
-            schema: nameSchema,
-        },
-        slug: {
-            value: slug,
-            schema: slugSchema,
-        },
-    });
+    const result = validateForm(
+        { name: body.get("name"), slug: body.get("slug") },
+        formSchema
+    );
 
-    if (!isFormValid(fieldErrors)) {
-        return { fieldErrors, fields };
+    if (!result.success) {
+        return {
+            fields: result.fields,
+            fieldErrors: result.fieldErrors,
+        };
     }
+
+    const { fields, fieldErrors } = result;
+    const { name, slug } = fields;
 
     const existingBrand = await prisma.brand.findFirst({
         where: {
--- app/types.ts
+++ app/types.ts
@@ -6,6 +6,6 @@ export type Override<OriginalType, OverrideType> = Omit<
 
 export type GetActionData<FormInputName extends string> = {
     formError?: string;
-    fieldErrors?: Partial<Record<FormInputName, string>>;
+    fieldErrors: Partial<Record<FormInputName, string | null>>;
     fields: Partial<Record<FormInputName, string | null>>;
 };
