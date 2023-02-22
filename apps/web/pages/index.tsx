c7b23ec1e0697fad8afa4822ecc4d91b41c44676
Adam Nicholson <adam.nicholson@sbs.com.au>
2023-02-22 17:32:32 +1000

    setup prisma

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,39 @@
 import { Button } from "ui";
+import prisma from "../lib/prisma";
 
-export default function Web() {
+type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
+
+export default function Web(props: Props) {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      {props.feed.map((post) => (
+        <div key={post.id}>
+          <>
+            {post.title}
+            {post.createdAt}
+            {post.author.name}
+          </>
+        </div>
+      ))}
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+
+  const feedWithStringData = feed.map((f) => ({
+    ...f,
+    createdAt: f.createdAt.getTime(),
+    updatedAt: f.updatedAt.getTime(),
+  }));
+
+  return {
+    props: { feed: feedWithStringData },
+  };
+};
---
+++ packages/database/prisma/README.md
@@ -0,0 +1,3 @@
+# Setting up postgres locally
+
+Follow this [guide](https://www.postgresqltutorial.com/postgresql-getting-started/install-postgresql-macos/)
